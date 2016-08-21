00632a32a1a7830581f6fe85c805eb0c4f24c11e
thrusanova <thrusanova@gmail.com>
2016-08-21 13:42:54 +0300

    exam preparation

---
+++ Exams/Exams/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Exams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            var regex = new Regex(@"\s*\|\s*");
+            var data = new Dictionary<string, List<string>>();
+            while (line!="report")
+            {
+                var input = regex.Split(line.Trim());
+                var name = Regex.Replace(input[0].Trim(), @"\s+", " ");
+                var country = Regex.Replace(input[1].Trim(), @"\s+", " ");
+                if (!data.ContainsKey(country))
+                {
+                    data.Add(country, new List<string>());
+                }
+                data[country].Add(name);
+                line = Console.ReadLine();
+
+            }
+
+            var sortedData = data.OrderByDescending(x => x.Value.Count);
+            foreach (var item in sortedData)
+            {
+                Console.WriteLine($"{item.Key} ({item.Value.Distinct().Count()} participants): {item.Value.Count} wins");
+            }
+        }
+    }
+}
---
+++ Exams/TextTransformer/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class TextTransformer
+{
+    public static void Main()
+    {
+        var CapturePattern = @"([$%&'])([^$%&']+)\1";
+
+
+        StringBuilder sb = new StringBuilder();
+
+        string input = Console.ReadLine();
+
+        while (input != "burp")
+
+        {
+            sb.Append(input);
+            input = Console.ReadLine();
+        }
+
+        string text = Regex.Replace(sb.ToString(), @"\s+", " ");
+        Regex stringMatcher = new Regex(CapturePattern);
+        var matches = stringMatcher.Matches(text);
+
+        StringBuilder result = new StringBuilder();
+
+        foreach (Match match in matches)
+        {
+            char specialSymbol = match.Groups[1].Value[0];
+            string capturedString = match.Groups[2].Value;
+            int stringLength = capturedString.Length;
+            if (specialSymbol == '$')
+            {
+                for (int i = 0; i < stringLength; i++)
+                {
+                    char currentSymbol = capturedString[i];
+                    char resultingChar;
+
+                    if (i % 2 == 0)
+                    {
+                        resultingChar = (char)(currentSymbol + 1);
+
+                    }
+                    else
+                    {
+                        resultingChar = (char)(currentSymbol - 1);
+                    }
+
+                    result.Append(resultingChar);
+                }
+            }
+                if (specialSymbol == '%')
+                {
+                    for (int i = 0; i < stringLength; i++)
+                    {
+                        char currentSymbol = capturedString[i];
+                        char resultingChar;
+
+                        if (i % 2 == 0)
+                        {
+                            resultingChar = (char)(currentSymbol + 2);
+
+                        }
+                        else
+                        {
+                            resultingChar = (char)(currentSymbol - 2);
+                        }
+
+                        result.Append(resultingChar);
+                    }
+                    result.Append(" ");
+                }
+                if (specialSymbol == '&')
+                {
+                    for (int i = 0; i < stringLength; i++)
+                    {
+                        char currentSymbol = capturedString[i];
+                        char resultingChar;
+
+                        if (i % 2 == 0)
+                        {
+                            resultingChar = (char)(currentSymbol + 3);
+
+                        }
+                        else
+                        {
+                            resultingChar = (char)(currentSymbol - 3);
+                        }
+
+                        result.Append(resultingChar);
+                    }
+                }
+
+                if (specialSymbol == '\'')
+                {
+                    for (int i = 0; i < stringLength; i++)
+                    {
+                        char currentSymbol = capturedString[i];
+                        char resultingChar;
+
+                        if (i % 2 == 0)
+                        {
+                            resultingChar = (char)(currentSymbol + 4);
+
+                        }
+                        else
+                        {
+                            resultingChar = (char)(currentSymbol - 4);
+                        }
+
+                        result.Append(resultingChar);
+                    }
+
+                   
+                }
+                Console.WriteLine(result);
+            }
+        }
+    }
